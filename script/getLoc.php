933d3c533f105d4d916201ded87b63e30294c632
Cosmo <daixianceng@gmail.com>
2015-03-02 23:29:26 +0800

    优化getLoc.php

--- script/getLoc.php
+++ script/getLoc.php
@@ -1,23 +1,25 @@
 <?php
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
-$pdo->exec('SET NAMES UTF8');
+$pdo = new PDO('sqlite:' . realpath('location.db'));
 
 $id = (int) isset($_GET['id']) ? $_GET['id'] : 0;
 
+$types = array(1 => 'country', 'state', 'city', 'region');
 $data = array(
 	'html' => '',
 	'status' => '0',
 	'type' => ''
 );
-$sql = "SELECT * FROM t_location WHERE parent_id={$id}";
-$result = $pdo->query($sql);
+$sql = "SELECT id, name FROM t_location WHERE parent_id={$id}";
+$result = $pdo->query($sql, PDO::FETCH_KEY_PAIR)->fetchAll();
 
-if ($result && $result->rowCount() > 0) {
+if (!empty($result)) {
 	$data['status'] = '1';
-	foreach ($result as $row) {
-		$data['html'] .= '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
-		$data['type'] = $row['type'];
+	foreach ($result as $key => $name) {
+		$data['html'] .= '<option value="' . $key . '">' . mb_convert_encoding($name, 'utf8', 'gbk') . '</option>';
 	}
+	
+	$type = $pdo->query("SELECT type FROM t_location WHERE parent_id={$id} LIMIT 1", PDO::FETCH_COLUMN, 0)->fetchColumn();
+	$data['type'] = $types[$type];
 }
 
 echo json_encode($data);
